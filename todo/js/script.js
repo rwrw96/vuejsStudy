b8287cd774a1041b17a188cc52bceb5efdbdf80b
watabe <kazuki137@gmail.com>
2021-03-08 11:35:09 +0900

    フォームが空白だった時データを表示しないバリデーションの追加

--- todo/js/script.js
+++ todo/js/script.js
@@ -6,11 +6,12 @@ var app = new Vue({
     },
     methods: {
         addItem: function (event) {
+            if(this.newItem == "") return ;
             var todo = {
                 item: this.newItem
             };
             this.todos.push(todo);
-            this.newItem = "";
+            this.newItem = ""
         }
     }
 
