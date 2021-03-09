259eaf533c75004a4f3a13abd11eaa61a8d29fd0
watabe <kazuki137@gmail.com>
2021-03-10 08:38:09 +0900

    算出プロパティにて名前の表記の学習

--- test/script.js
+++ test/script.js
@@ -2,25 +2,12 @@ var app = new Vue ({
     el: "#app",
     data: {
         // message: "hello,vue.js!"
-        km: 0,
-        m: 0,
-        mm: 0
+        firstName: "",
+        lastName: ""
     },
-    watch: {
-        km: function(value){
-            this.km = value
-            this.m = value * 1000
-            this.mm = value * 1000000
-        },
-        m: function(value){
-            this.km = value / 1000
-            this.m = value
-            this.mm = value * 1000
-        },
-        mm: function(value){
-            this.km = value / 1000000
-            this.m = value / 1000
-            this.mm = value
+    computed: {
+        fullName: function() {
+            return this.firstName + " " + this.lastName ;
         }
-    } 
+    }
 })
