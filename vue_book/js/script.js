619aa4b11831f58adf5f903ce38f181b99056e3e
watabe <kazuki137@gmail.com>
2021-03-11 10:47:47 +0900

    モンスターを追加するギミックを追加

--- vue_book/js/script.js
+++ vue_book/js/script.js
@@ -1,6 +1,23 @@
 var app = new Vue ({
     el: "#app",
     data: {
-        show: true
+        monster: "",
+        list: [
+            { id: 1, monster: "キマイラ", HP:300},
+            { id: 2, monster: "スライム", HP:100},
+            { id: 3, monster: "ケルベロス", HP:500}
+        ]
+    },
+    methods: {
+        addMonster: function(){
+            var max = this.list.reduce(function(a,b){
+                return a.id > b.id ? a.id : b.id
+            }, 0)
+            this.list.push({
+                id: max + 1,
+                monster: this.monster,
+                HP: 1000
+            })
+        }
     }
 })
